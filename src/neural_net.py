4a9385b04ac3fc78c8f076290dfd8234c325b931
PaMartini <paul.martini@web.de>
2023-04-17 21:33:44 +0200

    Added WineNet class

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,5 +1,6 @@
 
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 
@@ -20,7 +21,9 @@ class WineDataset(Dataset):
         return len(self.df)
 
 
-def load_wine_data(val_and_test: bool = False) -> Tuple[DataLoader, ...]:
+def get_data_loaders_wine_data(val_and_test: bool = False,
+                               batch_size: int = 1,
+                               shuffle: bool = False) -> Tuple[DataLoader, ...]:
     if val_and_test:
         traindf, valdf, testdf = data_pipeline_redwine(val_and_test=val_and_test)
         traindf = traindf.drop(columns=['quality'])
@@ -31,9 +34,9 @@ def load_wine_data(val_and_test: bool = False) -> Tuple[DataLoader, ...]:
         vald = WineDataset(valdf)
         testd = WineDataset(testdf)
 
-        train_loader = DataLoader(traind)
-        val_loader = DataLoader(vald)
-        test_loader = DataLoader(testd)
+        train_loader = DataLoader(traind, batch_size=batch_size, shuffle=shuffle)
+        val_loader = DataLoader(vald, batch_size=batch_size, shuffle=shuffle)
+        test_loader = DataLoader(testd, batch_size=batch_size, shuffle=shuffle)
 
         return train_loader, val_loader, test_loader
 
@@ -45,8 +48,42 @@ def load_wine_data(val_and_test: bool = False) -> Tuple[DataLoader, ...]:
         traind = WineDataset(traindf)
         testd = WineDataset(testdf)
 
-        train_loader = DataLoader(traind)
-        test_loader = DataLoader(testd)
+        train_loader = DataLoader(traind, batch_size=batch_size, shuffle=shuffle)
+        test_loader = DataLoader(testd, batch_size=batch_size, shuffle=shuffle)
 
         return train_loader, test_loader
+
+
+class WineNet(torch.nn.Module):
+    def __init__(self, in_size: int = 11, out_size: int = 1):
+        super().__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.net = nn.Sequential(
+            nn.Linear(in_features=self.in_size, out_features=2 * self.in_size),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=2 * self.in_size, out_features=6 * self.in_size),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=6 * self.in_size, out_features=6 * self.in_size),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=6 * self.in_size, out_features=2 * self.in_size),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=2 * self.in_size, out_features=self.out_size),
+        )
+
+        self.last_activation = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.net(x)
+        x = self.last_activation(x)
+        return x
+
+
 # Todo
+
+if __name__ == '__main__':
+    train_loader, _ = get_data_loaders_wine_data(val_and_test=False, batch_size=20, shuffle=True)
+    for item in train_loader:
+        print(item[0])
+        print(item[1])
+
