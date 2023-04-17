156c12f5b8c15e305fbc4bde83c6f9ea8761af27
PaMartini <paul.martini@web.de>
2023-04-17 23:43:02 +0200

    Debug

--- src/neural_net.py
+++ src/neural_net.py
@@ -84,7 +84,7 @@ class WineNet(torch.nn.Module):
 def save_ckp(model_state: dict, checkpoint_dir: str = "../checkpoints/") -> None:
     fp = os.path.join(checkpoint_dir, 'model_' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.pt')
     torch.save(model_state, fp)
-    print(f'Saved at model')
+    print(f'Saved model.')
 
 
 def train_loop(train_loader: DataLoader,
@@ -94,18 +94,25 @@ def train_loop(train_loader: DataLoader,
                loss_fct: Any,
                num_epochs: int = 100,
                save: bool = False,
-               checkpoint_dir: str = "../checkpoints/") -> Tuple[Type[nn.Module], np.ndarray, np.ndarray]:
+               checkpoint_dir: str = "../checkpoints/",
+               verbosity: bool = False) -> Tuple[Type[nn.Module], np.ndarray, np.ndarray]:
 
     train_losses = np.zeros(num_epochs)
     val_losses = np.zeros(num_epochs)
 
+    flatten = nn.Flatten(start_dim=0, end_dim=-1)
+
     for epoch in range(num_epochs):
+        if verbosity:
+            print(f"### Starting epoch {epoch + 1} of {num_epochs} ###")
         running_train_loss = 0
         for i, data in tqdm(enumerate(train_loader), total=len(train_loader)):
-            x = data[0]
-            y = data[1]
+            x = data[0].to(torch.float32)
+            y = data[1].to(torch.float32)
             model.train()
             model_out = model(x)
+            model_out = flatten(model_out)
+            optimizer.zero_grad()
             loss = loss_fct(model_out, y)
             loss.backward()
             optimizer.step()
@@ -114,22 +121,26 @@ def train_loop(train_loader: DataLoader,
 
         running_val_loss = 0
         for i, data in tqdm(enumerate(val_loader), total=len(val_loader)):
-            x = data[0]
-            y = data[1]
+            x = data[0].to(torch.float32)
+            y = data[1].to(torch.float32)
             model.eval()
             with torch.no_grad():
                 model_out = model(x)
+                model_out = flatten(model_out)
                 loss = loss_fct(model_out, y)
                 running_val_loss += loss.item()
 
         train_losses[epoch] = running_train_loss / len(train_loader)
         val_losses[epoch] = running_val_loss / len(val_loader)
 
+        if verbosity:
+            print(f"### Current train loss is {train_losses[epoch]} ###")
+            print(f"### Current validation loss is {val_losses[epoch]} ###")
+
     if save:
         model_state = {'state_dict': model.state_dict(),
                        'optimizer': optimizer.state_dict()}
         save_ckp(model_state=model_state, checkpoint_dir=checkpoint_dir)
-
 
     return model, train_losses, val_losses
 
@@ -146,11 +157,17 @@ def run_training():
                optimizer=optimizer,
                loss_fct=loss_fct,
                num_epochs=n_epochs,
-               save=True)
+               save=True,
+               verbosity=True)
+
+
+def check_model():
+    model = WineNet()
+    summary(model, input_size=(11,))
 
 
 if __name__ == '__main__':
 
-    model = WineNet()
-    summary(model, input_size=(11,))
+    run_training()
+    print('done')
 
